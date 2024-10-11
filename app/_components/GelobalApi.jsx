52d9b0b5811cb261f5f0cfb7129b2463b091a07b
Ali Shariatian <shariatian92@gmail.com>
2024-10-11 22:04:51 +0330

    fix: change json-server port

--- app/_components/GelobalApi.jsx
+++ app/_components/GelobalApi.jsx
@@ -1,16 +1,10 @@
 const { default: axios } = require("axios");
 
 const axiosClient = axios.create({
-    baseURL : "http://localhost:3000" ,
-    headers : {
-        "Content-Type" : "application-json" ,
-        
-    }
-})
+  baseURL: "http://localhost:8080",
+  headers: {
+    "Content-Type": "application-json",
+  },
+});
 
-
-export default axiosClient ;
-
-
-
-
+export default axiosClient;
