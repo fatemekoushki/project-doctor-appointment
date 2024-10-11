d06924ac6453e4ccd9abdd69cbc61bac98bd6825
fatemekoushki <fatemekoushki3@gmail.com>
2024-10-11 23:34:27 +0330

    part5

--- middleware.js
+++ middleware.js
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
+    res.setHeader('Access-Control-Allow-Origin', '*');  
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');  
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');  
     const { pathname } = request.nextUrl;
     const token = request.cookies.get("tokenLogin");
     if (pathname.startsWith("/details")) {
